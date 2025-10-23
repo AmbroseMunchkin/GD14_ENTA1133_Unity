9336013edf00ef7a6c300fed990bcb82eb9f1644
Arlo <lux.morte@yahoo.com>
2025-10-23 16:51:54 -0700

    Added Files for Dice logic

---
+++ ENTA-1133/Assets/Scripts/DiceGameScripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts
+{
+    internal class GameManager
+    {
+        public void ProgramStart()
+        {
+            Player.User(); //Decided to add this one first so it looks more clean
+            Intro(); 
+            RollOrDie();
+            Outro();
+        }
+        private void Intro()
+        {
+            Debug.Log("Let's roll some dice! May the odds be ever in your favor\n");
+        }
+        private void RollOrDie()
+        {
+            RandomTurn.Turn();
+        }
+        private static void Outro()
+        {
+            Debug.Log("Now this was a lot of rolling! So many possibilities with so few dice.\n");
+            Debug.Log("Thanks for giving this a try! Have a wonderful day!");
+        }
+    }
+}
---
+++ ENTA-1133/Assets/Scripts/DiceGameScripts/GameRunner.cs
@@ -0,0 +1,17 @@
+using GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts;
+using UnityEngine;
+
+public class GameRunner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.ProgramStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
