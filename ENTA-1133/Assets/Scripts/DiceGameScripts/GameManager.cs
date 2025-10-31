321c3eaa4146f137819464e89d4bd2cd06c55866
Arlo <lux.morte@yahoo.com>
2025-10-30 21:37:20 -0700

    Created prefabs
    Created the prefabs of wall, empty room and block of the wall

--- ENTA-1133/Assets/Scripts/DiceGameScripts/GameManager.cs
+++ ENTA-1133/Assets/Scripts/DiceGameScripts/GameManager.cs
@@ -1,32 +1,195 @@
 ﻿using System;
+using System.Data;
 using UnityEngine;
 
 namespace GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts
 {
-    internal class GameManager
+    internal class GameManager : MonoBehaviour
     {
+        public void Start()
+        {
+            Debug.Log("GameManager Constructor Start");
+
+
+            StartGame();
+
+            Debug.Log("GameManager Constructor End");
+        }
+
+        public void StartGame()
+        {
+            Debug.Log("Hello, World!");
+            Map gameMap = new Map();
+            gameMap.InitializeFlexible();
+        }
+        public static Player player = new Player();
+
+        //DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+        //private Map Map = new Map();
+        int x;
+        int y;
+
+        //private Room playerCurrentRoom;
+        //private Room playerLastRoom;
+
+        // constants
+        private const int MapX = 3;
+        private const int MapY = 3;
+
+        public static bool gameIsRunning = false;
+
         public void ProgramStart()
         {
-            //Player.User(); //Took this down since i dont need player username
-            Intro(); 
-            RollOrDie();
-            Outro();
+            // What does the game need to do Arlet?
+            // Say Hello
+            Intro();
+            // Get Player Name
+            player.User(); //Fixed how the intro is since it was rude to start by asking the player name
+            GameExplanation();
+
+            Console.WriteLine("Do you want to wander the world? Type \"yes\" to continue");
+            gameIsRunning = Console.ReadLine() == "yes";
+
+            // * GameLoop begins * * Ending on player reaching 0 hp || surrender || win condition
+
+            while (gameIsRunning)
+            {
+                // At the start of the loop set any variables that will need to reset when the game resets (i.e. play again)
+                InitializeNewGame();
+
+                // Enter the first room
+                //playerCurrentRoom.OnEnter(player);
+
+                // Setup is complete
+                // Begin the player loop
+                GameDecisionLoop();
+
+                // Ask the player if they want to play again
+                Console.WriteLine("Try again? Input \"yes\"");
+                gameIsRunning = Console.ReadLine() == "yes";
+            }
+
+            // Final goodbye message REGARDLESS OF WHETHER THEY WON OR LOST, this is just the final goodbye
+            Console.Write("The program will now close. Thank you.");
+        }
+
+        private void InitializeNewGame()
+        {
+            // Initialize Map
+            //Map.InitializeFlexible(MapX, MapY); //Initialized the map and assigned rooms
+
+            // Set the player current room
+            // MUST BE DONE AFTER MAKING THE MAP!
+            //playerCurrentRoom = Map.StartRoom(1, 1);
+
+            // TODO
+            // Set up the Player, reset player, default equipment, etc etc.
+            //player.Reset();
+        }
+
+        private void GameDecisionLoop()
+        {
+            while (gameIsRunning)
+            {
+                bool validInput;
+                string choice;
+                // Input validation loop
+                do
+                {
+                    choice = GetPlayerChoiceForCurrentStep();
+                    validInput = choice != "Error";
+                }
+                while (validInput == false);
+
+                ResolveCurrentChoice(choice);
+                // What CAN happen based on their choices?
+                // 1 Move
+                // 2 Inventory
+                // 3 Surrender
+                // 4 Test fight
+                // these will have to resolve however they need to
+                // then the game should should come back to this point
+
+
+                // If the player dies during a combat, then gameIsRunning will be false, and we will not loop.
+            }
         }
         private void Intro()
-        {
-            DateTime today = DateTime.Now.Date;
-            Debug.Log("Welcome to Dice Rolling! My name is Arlet Alcaraz and today is " + today);
-            Debug.Log("Let's roll some dice! May the odds be ever in your favor\n");
+        { //This greats the player and shows the game name
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine("Hello, hello hello! Uh, welcome to");
+            Console.WriteLine(" ___   ___   _     _         ___   ___       ___   _   ____ \r\n| |_) / / \\ | |   | |       / / \\ | |_)     | | \\ | | | |_  \r\n|_| \\ \\_\\_/ |_|__ |_|__     \\_\\_/ |_| \\     |_|_/ |_| |_|__ \r\n                                                            \r\n                                                            \r\n                                                            ");
+            Console.WriteLine("A place where your soul is at stake!");
+            //Console.WriteLine("Today is: " + today);
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine();
         }
-        private void RollOrDie()
-        {
-            RandomTurn randomTurn = new RandomTurn();
-            randomTurn.Turn();
+
+        private void Outro()
+        { //This is where we say goodbye to the player
+            Console.WriteLine();
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine("Thank you for keeping me entertained dear wandering soul~");
+            Console.WriteLine("You fought for your soul with great bravery; you can keep it...");
+            Console.WriteLine("░        ░░░      ░░░       ░░░░░░░░░   ░░░  ░░░      ░░░  ░░░░  ░\r\n▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒▒    ▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒  ▒  ▒\r\n▓      ▓▓▓▓  ▓▓▓▓  ▓▓       ▓▓▓▓▓▓▓▓▓  ▓  ▓  ▓▓  ▓▓▓▓  ▓▓        ▓\r\n█  ████████  ████  ██  ███  █████████  ██    ██  ████  ██   ██   █\r\n█  █████████      ███  ████  ████████  ███   ███      ███  ████  █\r\n                                                                  ");
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
         }
-        private static void Outro()
+        private void GameExplanation()
+        { //The rules of the game
+            Console.WriteLine();
+            Console.WriteLine("You can travel this 3x3 dungeon full of treasure chests!\nBut be careful, there's eyes watching all your movements...\n");
+            Console.WriteLine("You will start with a sword and a maze, but you can find potion in the chests, those will be helpful~\n");
+            //Console.WriteLine($"Be aware " + player.username + " that once a potion is used it will disappear never to be seen again.\n");
+        }
+
+        public string GetPlayerChoiceForCurrentStep()
         {
-            Debug.Log("Now this was a lot of rolling! So many possibilities with so few dice.\n");
-            Debug.Log("Thanks for giving this a try! Have a wonderful day!");
+            // Show the player all their options.
+            // Validate their input
+            // Get their decision
+            Console.WriteLine("=======");
+            //Console.WriteLine("Rooms Visited: " + player.NumberOfRoomsVisited + " of " + Map.NumberOfRooms);
+            //Console.WriteLine("You are in " + playerCurrentRoom.GetNameRoom() + ", what do you want to do now?\n"); //I give the player its options
+            Console.WriteLine("1.-Move\n2.-Check your inventory\n3.-Give up your soul to me\n4.-Search the room");
+
+            string decision = Console.ReadLine();
+            switch (decision)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    return decision;
+                default:
+                    return "Error";
+            }
+        }
+
+        public void ResolveCurrentChoice(string decision)
+        {
+            switch (decision)
+            {
+                case "1":
+                    //playerCurrentRoom = Map.MoveDecisionLoop(playerCurrentRoom, player);
+                    break;
+                case "2":
+                    //player.Inventory(); //Here it prints the player inventory
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    gameIsRunning = false;
+                    Outro(); //Ends the run
+                    break;
+                case "4":
+                    {
+                        //playerCurrentRoom.SearchRoom();
+                        break;
+                    }
+                default:
+                    Console.WriteLine("I couldn't resolve the input of " + decision);
+                    break;
+            }
         }
     }
 }
---
+++ ENTA-1133/Assets/Scripts/DiceGameScripts/Map.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts
+{
+    internal class Map
+    {
+        //private Room[,] _map;
+
+        //public int NumberOfRooms => _map.Length;
+        int mapSize = 20;
+        public int MapSize => mapSize;
+        public void InitializeFlexible()
+        {
+            //_map = new Room[x, y];
+
+            for (int x = 0; x < mapSize; x++) 
+            {
+                for (int z = 0; z < mapSize; z++)
+                {
+                    //SetRoom(i, j); //Room gets selected and assigned
+                    //The rooms get linked here
+                    var mapRoomRepresentation = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    mapRoomRepresentation.transform.position = new Vector3(x, 0, z);
+                }
+            }
+            //for (int i = 0; i < x; i++)
+            //{
+            //    for (int j = 0; j < y; j++)
+            //    {
+            //        //The rooms get linked here
+            //        //Room currentRoom = _map[i, j];
+            //        if (i > 0) currentRoom.NorthRoom = _map[i - 1, j];       //Checks if there can be a north room
+            //        if (i < y - 1) currentRoom.SouthRoom = _map[i + 1, j];   //Checks if there can be a south room 
+            //        if (j > 0) currentRoom.WestRoom = _map[i, j - 1];       //Checks if there can be a west room
+            //        if (j < x - 1) currentRoom.EastRoom = _map[i, j + 1];   //Checks if there can be an east room
+            //    }
+            //}
+        }
+
+        
+        //public Room StartRoom(int x , int y) //Here is where i make the player start location
+        //{
+        //    return _map[1, 1]; //Returns the middle room as the start
+        //}
+        //public Room GetRoom(int x, int y)
+        //{
+        //    return _map[x, y];
+        //}
+        public void SetRoom(int x, int y)
+        {
+            //Room room;
+            System.Random rand = new System.Random(); //Does a random to decide if the room will have a combat or treasure
+            int randomNum = rand.Next(0, 2);
+            if (randomNum == 1)
+            {
+                //room = new TreasureRoom();
+            }
+            else
+            {
+                //room = new CombatRoom();
+            }
+            //_map[x, y] = room;
+        }
+
+        //public void MoveRooms(Room currentRoom)
+        //{
+        //    Console.WriteLine("Where do you want to go?\n");
+        //    Console.WriteLine("North, South, West or East?\n");
+        //    string roomSelect = Console.ReadLine();
+        //    switch (roomSelect)
+        //    {
+        //        case "North":
+        //            if (currentRoom.NorthRoom == null)
+        //            {
+        //                Console.WriteLine("You can't go up!");
+        //                MoveRooms(currentRoom);
+        //            }
+        //            else
+        //            {
+        //                currentRoom.OnExit();
+        //                currentRoom = currentRoom.NorthRoom;
+        //                currentRoom.OnEnter();
+        //            }
+        //            break;
+        //        case "South":
+        //            if (currentRoom.SouthRoom == null)
+        //            {
+        //                Console.WriteLine("You can't go down!");
+        //                MoveRooms(currentRoom);
+        //            }
+        //            else
+        //            {
+        //                currentRoom.OnExit();
+        //                currentRoom = currentRoom.SouthRoom;
+        //                currentRoom.OnEnter();
+        //            }
+        //            break;
+        //        case "West":
+        //            if (currentRoom.WestRoom == null)
+        //            {
+        //                Console.WriteLine("You can't go to the left!");
+        //                MoveRooms(currentRoom);
+        //            }
+        //            else
+        //            {
+        //                currentRoom.OnExit();
+        //                currentRoom = currentRoom.WestRoom;
+        //                currentRoom.OnEnter();
+        //            }
+        //            break;
+        //        case "East":
+        //            if (currentRoom.EastRoom == null)
+        //            {
+        //                Console.WriteLine("You can't go to the right!");
+        //                MoveRooms(currentRoom);
+        //            }
+        //            else
+        //            {
+        //                currentRoom.OnExit();
+        //                currentRoom = currentRoom.EastRoom;
+        //                currentRoom.OnEnter();
+        //            }
+        //            break;
+        //        default:
+        //            Console.WriteLine("Thats not a valid option, please try again");
+        //            MoveRooms(currentRoom);
+        //            break;
+        //    }
+        //}
+
+        //internal Room MoveDecisionLoop(Room currentRoom, Player user)
+        //{
+        //    bool validInput;
+        //    bool isRoomInDirection;
+        //    string choice;
+        //    do
+        //    {
+        //        // Input validation loop
+        //        do
+        //        {
+        //            choice = GetDirection(currentRoom);
+        //            validInput = choice != "Error";
+        //        }
+        //        while (validInput == false);
+
+        //        if (choice == "Cancel") return currentRoom;
+
+        //        // Is there a room in that direction?
+        //        isRoomInDirection = IsRoomInDirection(currentRoom, choice);
+        //        if (isRoomInDirection == false)
+        //            Console.WriteLine("No room that way!");
+        //    }
+        //    while (validInput == false);
+
+        //    currentRoom.OnExit(); // 1,1
+        //    currentRoom = MovePlayer(currentRoom, choice);
+        //    currentRoom.OnEnter(user);
+        //    return currentRoom;
+        //}
+
+        //public string GetDirection(Room currentRoom)
+        //{
+        //    Console.WriteLine("Where do you want to go?\n");
+        //    Console.WriteLine("North, South, West or East?\n");
+
+        //    string decision = Console.ReadLine();
+        //    switch (decision)
+        //    {
+        //        case "North":
+        //        case "South":
+        //        case "East":
+        //        case "West":
+        //        case "Cancel":
+        //            return decision;
+        //        default:
+        //            return "Error";
+        //    }
+        //}
+
+        //public bool IsRoomInDirection(Room currentRoom, string direction)
+        //{
+        //    switch (direction)
+        //    {
+        //        case "North":
+        //            return currentRoom.NorthRoom != null;
+        //        case "South":
+        //            return currentRoom.SouthRoom != null;
+        //        case "East":
+        //            return currentRoom.EastRoom != null;
+        //        case "West":
+        //            return currentRoom.WestRoom != null;
+        //        default:
+        //            Console.WriteLine("IsRoomInDirection: Unknown direction! " + direction);
+        //            break;
+        //    }
+        //    return false;
+        //}
+
+        //public Room? MovePlayer(Room currentRoom, string direction)
+        //{
+        //    switch (direction)
+        //    {
+        //        case "North":
+        //            return currentRoom.NorthRoom;
+        //        case "South":
+        //            return currentRoom.SouthRoom;
+        //        case "East":
+        //            return currentRoom.EastRoom;
+        //        case "West":
+        //            return currentRoom.WestRoom;
+        //        default:
+        //            Console.WriteLine("MovePlayer: Unknown direction! " + direction);
+        //            return null;
+        //    }
+        //}
+
+        //public void Move()
+        //{
+        //    Console.WriteLine("You try to move but fail");
+        //}
+    }
+
+}
